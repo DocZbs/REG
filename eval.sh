9fc0b593de0ed6208182e23d37dd860dcc139195
zbs <2733422728@qq.com>
2025-10-05 10:48:24 +0800

    oom

--- eval.sh
+++ eval.sh
@@ -10,27 +10,27 @@ CFG_SCALE=2.3
 CLS_CFG_SCALE=2.3
 GH=0.85
 
-export NCCL_P2P_DISABLE=1
+# export NCCL_P2P_DISABLE=1
 
-python -m torch.distributed.launch --master_port=$random_number --nproc_per_node=$NUM_GPUS generate.py \
-  --model SiT-B/2 \
-  --num-fid-samples 50000 \
-  --ckpt ${SAVE_PATH}/checkpoints/${STEP}.pt \
-  --path-type=linear \
-  --encoder-depth=8 \
-  --projector-embed-dims=768 \
-  --per-proc-batch-size=64 \
-  --mode=sde \
-  --num-steps=${NUM_STEP} \
-  --cfg-scale=${CFG_SCALE} \
-  --cls-cfg-scale=${CLS_CFG_SCALE} \
-  --guidance-high=${GH} \
-  --sample-dir ${SAVE_PATH}/checkpoints \
-  --cls=1152
+# python -m torch.distributed.launch --master_port=$random_number --nproc_per_node=$NUM_GPUS generate.py \
+#   --model SiT-B/2 \
+#   --num-fid-samples 50000 \
+#   --ckpt ${SAVE_PATH}/checkpoints/${STEP}.pt \
+#   --path-type=linear \
+#   --encoder-depth=8 \
+#   --projector-embed-dims=768 \
+#   --per-proc-batch-size=64 \
+#   --mode=sde \
+#   --num-steps=${NUM_STEP} \
+#   --cfg-scale=${CFG_SCALE} \
+#   --cls-cfg-scale=${CLS_CFG_SCALE} \
+#   --guidance-high=${GH} \
+#   --sample-dir ${SAVE_PATH}/checkpoints \
+#   --cls=1152
 
 
 python ./evaluations/evaluator.py \
-    --ref_batch your_path/VIRTUAL_imagenet256_labeled.npz \
+    --ref_batch evaluations/VIRTUAL_imagenet256_labeled.npz \
     --sample_batch ${SAVE_PATH}/checkpoints/SiT-${MODEL_SIZE}-2-${STEP}-size-256-vae-ema-cfg-${CFG_SCALE}-seed-0-sde-${GH}-${CLS_CFG_SCALE}.npz \
     --save_path ${SAVE_PATH}/checkpoints \
     --cfg_cond 1 \
